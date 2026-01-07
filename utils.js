ff879a726d216ff64bd0ed6640ebdfc2e70f33a9
THECODERrs <117234237+THECODERrs@users.noreply.github.com>
2026-01-06 19:52:14 -0500

    Add utility functions for HTML handling

---
+++ utils.js
@@ -0,0 +1,19 @@
+function isHtmlExtension(file) {
+  return /\.html?$/i.test(file.name);
+}
+
+function isHtmlContent(text) {
+  return /<!doctype html>|<html[\s>]/i.test(text);
+}
+
+function findActivationKeys(html) {
+  const regex = /activationKey\s*:\s*["'`](.*?)["'`]/g;
+  const keys = [];
+  let m;
+  while ((m = regex.exec(html))) keys.push(m[1]);
+  return keys;
+}
+
+function generateId() {
+  return "ac_" + Math.random().toString(36).slice(2);
+}
